da511f4f1442e4c73a18b8e7a97c4c5ea7af2fbb
“ncedile <ncedile.nkonyana@sovtech.com>
2025-02-22 17:40:16 +0200

    fix: refactor syntax

--- src/hooks/useProduct.ts
+++ src/hooks/useProduct.ts
@@ -4,13 +4,13 @@ const { VITE_FAKE_STORE_BASE_URL } = import.meta.env;
 
 export default function useProduct(id: string) {
   const productKey = id ? `/products/${id}` : null;
-  const { data } = useSWR(productKey, async (url) => {
+  const { data: product } = useSWR(productKey, async (url) => {
     const response = await fetch(`${VITE_FAKE_STORE_BASE_URL}${url}`);
     return response.json();
   });
 
   return {
-    product: data,
-    isLoading: !data,
+    product,
+    isLoading: !product,
   };
 }
--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -4,15 +4,13 @@ const { VITE_FAKE_STORE_BASE_URL } = import.meta.env;
 
 export default function useProducts() {
   const productKey = "/products";
-  const { data } = useSWR(productKey, async (url) => {
+  const { data: products } = useSWR(productKey, async (url) => {
     const response = await fetch(`${VITE_FAKE_STORE_BASE_URL}${url}`);
     return response.json();
   });
 
-  console.log("--------data------", data);
-
   return {
-    products: data,
-    isLoading: !data,
+    products,
+    isLoading: !products,
   };
 }
